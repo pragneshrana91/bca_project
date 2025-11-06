2d0d08ba005afff1eb0ffec0ab445cd6714da528
pragneshrana91 <95336863+pragneshrana91@users.noreply.github.com>
2025-11-06 15:30:34 +0530

    Update bca_project_main.tf
    Add more modification in main.tf

--- terraform/bca_project_main.tf
+++ terraform/bca_project_main.tf
@@ -84,7 +84,23 @@ resource "aws_instance" "bca_ec2" {
       "sudo tar -xvzf apache-maven-3.9.11-bin.tar.gz",
       "sudo mv apache-maven-3.9.11 maven",
       "cd /opt/maven/bin/",
-      "sudo ./mvn -v"
+      "sudo ./mvn -v",
+      #"sudo su",
+      #"cd ~/",
+      "echo 'M2_HOME=/opt/maven' | sudo tee -a /root/.bash_profile",
+      "echo 'M2=/opt/maven/bin' | sudo tee -a /root/.bash_profile",
+      "echo 'JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64' | sudo tee -a /root/.bash_profile",
+      "echo 'PATH=$PATH:$HOME/bin:$JAVA_HOME:$M2_HOME:$M2' | sudo tee -a /root/.bash_profile",
+      "sudo source /root/.bash_profile",
+      "sudo cat /root/.bash_profile",
+      #"echo $PATH"
+      #"echo 'cd /opt/maven/bin/' | sudo tee -a /root/.bash_profile",
+      #"vi .bash_profile"
+      #M2_HOME=/opt/maven/
+      #M2=/opt/maven/bin/
+      #JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64
+      #PATH=$PATH:$HOME/bin:$JAVA_HOME:$M2_HOME:$M2
+      "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
     ]
 
     connection {
@@ -100,6 +116,6 @@ resource "aws_instance" "bca_ec2" {
   }
 }
 
-output "bca_ec2" {
-  value = aws_instance.bca_ec2.public_ip
+output "bca_ec2_url" {
+  value = "http://${aws_instance.bca_ec2.public_ip}:8080"
 }
